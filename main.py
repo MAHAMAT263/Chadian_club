044669364ff1f06930acac2261cf8394115d1d66
MAHAMAT263 <m.ahmat@alustudent.com>
2025-05-03 16:29:13 +0100

    my new file

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello Hassan")
+def coffe_shope():
+    available_coffe = ['Black coffe', 'Milk tea' , 'Milk Chocolate']
+    for i in available_coffe:
+        print(i)
+    X = input('Choose one of the avialbel coffe: ')
+    available_coffe.append(X)
+    for b in available_coffe:
+        c = available_coffe.index(b)
+        print(c,b)
+coffe_shope()
