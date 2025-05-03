1371446eb6d0097d2e1cfa80f379074ab5bfd2b6
MAHAMAT263 <m.ahmat@alustudent.com>
2025-05-03 17:23:18 +0100

    g

--- Exo_1.py
+++
@@ -1,8 +0,0 @@
-# Python program to find out what version of Python you are using.
-import sys  # Import the sys module to access system-specific parameters and functions
-
-# Print the Python version to the console
-print("Python version")
-
-# Use the sys.version attribute to get the Python version and print it
-print(sys.version)
